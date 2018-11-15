74003caca4cdbb69d0f28fd075bd00ae5ac15a2b
Asmit Gaurav Srivastav <Asmit@Etechaces.com>
2018-11-15 15:36:35 +0530

    key

--- config/keys.js
+++ config/keys.js
@@ -2,10 +2,10 @@ module.exports = {
   mongoURI: 'mongodb://10.0.8.62:27017/devconnector',
   secretOrKey: 'adadsad',
   mysqldb:{
-    host: 'localhost',
-    user: 'root',
+    host: '10.0.8.78',
+    user: 'pbsupport',
     port: '3306',
-    password: 'password',
+    password: 'pblive@1234@',
     database: 'ticketsystem'
 }
 };
